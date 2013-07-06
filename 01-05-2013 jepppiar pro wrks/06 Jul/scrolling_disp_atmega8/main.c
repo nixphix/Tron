08a71951e8f24c779cb59c24655524b21a607d08
Prabakaran <k_prabakaran@hotmail.com>
2013-07-06 22:59:57 +0530

    Working on First Service
    Tested Scrolling Display... gonna make some changes to src to fit Main
    brd controller some port changes

---
+++ 01-05-2013 jepppiar pro wrks/06 Jul/scrolling_disp_atmega8/main.c
@@ -0,0 +1,170 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <string.h>
+
+#include "font5x7.h"
+
+volatile PGM_P ptr=smallFont;
+
+volatile uint16_t ii,iii;
+
+//Message to display
+volatile char string[]="TEAMABCD";
+volatile uint8_t len;
+
+
+#define DISP_ROW_CNT 7
+#define DISP_COL_CNT 40// it takes 7 columns for a character but frst 6 is only data and next 1 is null
+
+#define HC595_PORT 	PORTB	
+#define HC595_DDR 	DDRB
+
+#define HC595_DS_POS PB0
+#define HC595_SH_CP_POS PB1
+#define HC595_ST_CP_POS PB2
+
+#define HC595DataHigh() (HC595_PORT|=(1<<HC595_DS_POS))
+#define HC595DataLow() (HC595_PORT&=(~(1<<HC595_DS_POS)))
+
+void HC595Init(void);
+void HC595Pulse(void);
+void HC595Latch(void);
+void SelectRow(uint8_t);
+
+void HC595Init()
+{
+	HC595_DDR|=((1<<HC595_SH_CP_POS)|(1<<HC595_ST_CP_POS)|(1<<HC595_DS_POS));
+}
+
+
+
+void HC595Pulse()
+{
+	
+	HC595_PORT|=(1<<HC595_SH_CP_POS);
+	HC595_PORT&=(~(1<<HC595_SH_CP_POS));
+}
+
+void HC595Latch()
+{
+	HC595_PORT|=(1<<HC595_ST_CP_POS);
+	_delay_loop_1(1);
+	HC595_PORT&=(~(1<<HC595_ST_CP_POS));
+	_delay_loop_1(1);
+}
+
+
+
+void SelectRow(uint8_t r)
+{
+	
+	PORTD&=(~(1<<PD7));
+	PORTD&=(~(1<<PD6));
+	PORTC&=0x00;
+
+	switch(r)
+	{
+		case 0:
+			PORTC|=(1<<PC0);
+			break;
+		case 1:
+			PORTC|=(1<<PC1);
+			break;
+		case 2:
+			PORTC|=(1<<PC2);
+			break;
+		case 3:
+			PORTC|=(1<<PC3);
+			break;
+		case 4:
+			PORTC|=(1<<PC4);
+			break;
+		case 5:
+			PORTD|=(1<<PD6);
+			break;
+		case 6:
+			PORTD|=(1<<PD7);
+			break;
+	}
+}
+		
+int main(void)
+{
+	
+	TIMSK=(1<<TOIE1); 
+	TCNT1=0xFFC0; 
+	TCCR1B = (1<<CS12)|(0<<CS11)|(0<<CS10); 
+	DDRC=0xFF;
+	DDRD=0XF0;
+	sei();
+	HC595Init();
+	len=strlen(string);
+	ii=iii=0;
+	while(1)
+	{
+	}
+	return 0;
+	
+}
+
+ISR(TIMER1_OVF_vect)
+{
+	PORTC=0x00;
+	PORTD&=(~(1<<PD7));
+	PORTD&=(~(1<<PD6));
+	TCNT1=0xFFC0;	
+	static uint8_t row;
+	static uint16_t cnt=0;cnt++;
+	
+	int8_t col;
+
+	int8_t chr = 0;						//iii;
+	int8_t m=0;							//ii;
+
+	for(col=0;col<DISP_COL_CNT;col++)
+	{
+		uint8_t data;
+		
+		if(m!=5)
+		{
+			
+			data=pgm_read_byte(( ptr+((string[chr]-' ')*5)+m));
+		}
+		else  
+		{
+			data=0x00;
+		}
+	
+		if((data & (1<<row)))
+			HC595DataHigh();
+		else
+			HC595DataLow();
+
+		HC595Pulse();
+		
+
+		m++;
+
+		if(m==6) // the number of columns for a single character
+		{
+			chr++;
+			m=0;
+
+			if(chr >=len)
+				{chr=0;}
+		}			
+		
+	}
+	HC595Latch();
+	SelectRow(row);
+	
+
+	row++;
+	if(row==DISP_ROW_CNT)
+	{
+		
+		row=0;
+	}
+}
